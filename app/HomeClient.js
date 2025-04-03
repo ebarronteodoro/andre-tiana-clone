2fed98e3324f7a90c0248b5d5e82924b8eb48dd1
axel <axelcaverocontacto@gmail.com>
2025-04-03 02:05:35 -0500

    UPDATE: Enhance mobile responsiveness by implementing image toggle for mobile devices and adjusting layout for larger screens

--- app/HomeClient.js
+++ app/HomeClient.js
@@ -10,6 +10,8 @@ export default function HomeClient() {
     const colors = ["#1c1547", "#521549", "#1a47c4", "#788988"];
     const [currentColor, setCurrentColor] = useState(colors[0]);
     const [prevColor, setPrevColor] = useState(colors[0]);
+    const [showAndrea, setShowAndrea] = useState(true);
+    const [isMobile, setIsMobile] = useState(false);
   
     useEffect(() => {
       let index = 0;
@@ -24,9 +26,37 @@ export default function HomeClient() {
       return () => clearInterval(interval);
     }, [currentColor, colors]);
 
+    useEffect(() => {
+      const handleResize = () => {
+        const isMobileDevice = window.matchMedia("(max-width: 768px)").matches;
+        setIsMobile(isMobileDevice);
+        console.log(`Is mobile: ${isMobileDevice}`); // Imprime si es móvil o no
+      };
+
+      // Verificar el tamaño inicial de la pantalla
+      handleResize();
+
+      // Escuchar cambios en el tamaño de la pantalla
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
+
+    useEffect(() => {
+      if (isMobile) {
+        const interval = setInterval(() => {
+          setShowAndrea((prev) => !prev);
+        }, 3000); // Cambia cada 3 segundos
+
+        return () => clearInterval(interval);
+      }
+    }, [isMobile]);
+
   return (
     <main className='min-h-screen !relative'>
-      <section className='!relative w-full h-auto bg-[#1a47c4] pb-8'>
+      <section className='!relative w-full h-auto bg-[#1a47c4] p-8'>
       <h1 className='m-x-auto text-4xl text-white text-center font-cardo'>
           Welcome to our Gallery
         </h1>
@@ -35,37 +65,89 @@ export default function HomeClient() {
           
           <TypewriterHeader text='Where Design Meets Art' />
         </span>
-        <div className='flex inset-0 w-full h-auto max-w-[1440px] mx-auto justify-center gap-[100px] sm:gap-[16px] md:gap-[32px] lg:gap-[64px]'>
-          <picture className='w-full max-w-[300px] relative flex items-center'>
-            <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center'>
-              Andrea Loarte
-            </span>
-            <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center'>
-              CEO &
-            </span>
-            <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center'>
-              Director de Arte"
-            </span>
-            <Image
-              className='w-full h-auto object-cover animate-fade animate-infinite animate-duration-[3000ms] animate-ease-in-out animate-alternate animate-fill-backwards'
-              src='/andrea-foto.png'
-              alt='Foto Silueta Andrea'
-              width={1920}
-              height={1080}
-            />
-          </picture>
-          <picture className='w-full max-w-[300px] relative flex items-center'>
-            <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center'>
-              <TypewriterHeader text="and I'm Tatiana Lopez" />
-            </span>
-            <Image
-              className='w-full h-auto object-cover'
-              src='/susana-foto.png'
-              alt='Foto Silueta Tiana'
-              width={1920}
-              height={1080}
-            />
-          </picture>
+        <div className='flex inset-0 w-full h-auto max-w-[1440px] mx-auto justify-center md:gap-16'>
+          {isMobile ? (
+            // Mostrar solo una imagen a la vez en dispositivos móviles
+            showAndrea ? (
+              <picture className='w-full max-w-[300px] relative flex items-center animate-fade animate-infinite animate-duration-[3000ms] animate-ease-in-out animate-alternate animate-fill-backwards'>
+                <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center'>
+                  Andrea Loarte
+                </span>
+                <span className='absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center top-[75%]'>
+                  CEO &
+                </span>
+                <span className='absolute w-[118px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center top-[80%]'>
+                  Director de Arte
+                </span>
+                <Image
+                  className='w-full h-auto object-cover '
+                  src='/andrea-foto.png'
+                  alt='Foto Silueta Andrea'
+                  width={1920}
+                  height={1080}
+                />
+              </picture>
+            ) : (
+              <picture className='w-full max-w-[300px] relative flex items-center animate-fade animate-infinite animate-duration-[3000ms] animate-ease-in-out animate-alternate animate-fill-backwards'>
+                <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center'>
+                  Tatiana Lopez
+                </span>
+                <span className='absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center top-[75%]'>
+                  Co-Fundadora &
+                </span>
+                <span className='absolute w-[118px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center top-[80%]'>
+                  Asesora Creativa
+                </span>
+                <Image
+                  className='w-full h-auto object-cover'
+                  src='/susana-foto.png'
+                  alt='Foto Silueta Tiana'
+                  width={1920}
+                  height={1080}
+                />
+              </picture>
+            )
+          ) : (
+            // Mostrar ambas imágenes al mismo tiempo en pantallas más grandes
+            <>
+              <picture className='w-full max-w-[300px] relative flex items-center '>
+                <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center'>
+                  Andrea Loarte
+                </span>
+                <span className='absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center top-[75%]'>
+                  CEO &
+                </span>
+                <span className='absolute w-[118px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center top-[80%]'>
+                  Director de Arte
+                </span>
+                <Image
+                  className='w-full h-auto object-cover '
+                  src='/andrea-foto.png'
+                  alt='Foto Silueta Andrea'
+                  width={1920}
+                  height={1080}
+                />
+              </picture>
+              <picture className='w-full max-w-[300px] relative flex items-center '>
+                <span className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center'>
+                  Tatiana Lopez
+                </span>
+                <span className='absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center top-[75%]'>
+                  Co-Fundadora &
+                </span>
+                <span className='absolute w-[118px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-cardo text-center top-[80%]'>
+                  Asesora Creativa
+                </span>
+                <Image
+                  className='w-full h-auto object-cover'
+                  src='/susana-foto.png'
+                  alt='Foto Silueta Tiana'
+                  width={1920}
+                  height={1080}
+                />
+              </picture>
+            </>
+          )}
         </div>
         
 
@@ -101,7 +183,7 @@ export default function HomeClient() {
               alt="Foto Marco"
             />
           </picture>
-          <span className='block text-[#1a47c4] mt-[32px] text-center font-cardo m-x-auto md:flex md:items-center'>
+          <span className='block text-[#1a47c4] mt-[32px] text-center font-cardo m-x-auto md:flex md:items-center '>
           <Image
               className='w-[40px] inline-block h-auto object-cover'
               src='/comillas.png'
