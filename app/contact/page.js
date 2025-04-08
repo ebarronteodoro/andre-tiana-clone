5e24d951b4f820475a317e12c1c19bb81b71b179
axel <axelcaverocontacto@gmail.com>
2025-04-07 19:52:44 -0500

    UPDATE: Adjust heading width for better responsiveness on the Contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -40,7 +40,7 @@ export default async function Home() {
               alt='Imagen de contacto'
               className='h-auto object-contain absolute -translate-y-1/2 top-[20%] left-6 x5c:w-[32px] md:left-10 lg:left-16 lg:w-[48px] '
             />
-            <h1 className='absolute -translate-y-1/2 top-[50%] left-6 text-white font-cardo w-[200px] text-[80%] x5c:text-[1rem] x5c:w-[300px] sm:w-[350px] sm:text-[1.5rem] md:left-10 lg:left-16'>
+            <h1 className='absolute -translate-y-1/2 top-[50%] left-6 text-white font-cardo w-[200px] text-[80%] x5c:text-[1rem] x5c:w-[262px] sm:text-[1.5rem] md:left-10 lg:left-16'>
               Agenda una videollamada gratuita para platicar de tu proyecto
             </h1>
             <Link href='#' target='_blank' className='bg-[#1a47c4] block w-fit  text-white py-1 px-2 rounded-[10px] text-xs absolute -translate-y-1/2 bottom-[8%] left-6 x4c:bottom-[15%] sm:text-[.8rem] md:left-10 lg:text-[16px] lg:left-16 lg:px-4 lg:py-2'>
