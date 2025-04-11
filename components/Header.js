9567ccfb09883995a84b0f6ef5fbff5b636c778a
ebarronteodoro <ebarronteodoro@hotmail.com>
2025-04-11 17:11:17 -0500

    UPDATE: Replace Image component with img tag in Header; adjust loading attribute for improved performance and maintain visual consistency.

--- components/Header.js
+++ components/Header.js
@@ -103,13 +103,11 @@ export default function Header () {
           rel={pathname === '/brochure' ? 'noopener noreferrer' : undefined}
           className='relative z-50'
         >
-          <Image
+          <img
             src={styleConfig.logoSrc}
             alt='Logo Andrea Loarte Design Studio'
-            width={224}
-            height={28}
-            priority
             className='h-auto object-contain w-44 md:w-56 aspect-[8/1]'
+            loading='eager'
           />
         </Link>
         <nav className='hidden md:flex items-center space-x-8'>
