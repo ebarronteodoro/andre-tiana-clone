383341cc6dbae7ec24e082f2748dd98c231a8fb4
Esteban Barrón <ebarronteodoro@hotmail.com>
2025-04-13 23:03:35 -0500

    UPDATE: actualizar enlace del botón de Instagram y ajustar estilos para mejorar la presentación

--- components/InstagramButton.js
+++ components/InstagramButton.js
@@ -4,20 +4,20 @@ import Link from 'next/link'
 export default function InstagramButton () {
   return (
     <Link
-      href='https://www.instagram.com'
+      href='https://www.instagram.com/andrea.loarte.s/'
       target='_blank'
       rel='noopener noreferrer'
       className='fixed bottom-4 right-4 z-50'
     >
-      <div className='relative w-16 h-16 flex items-center justify-center'>
+      <div className='relative size-20 flex items-center justify-center'>
         <span className='absolute inset-0 rounded-full bg-[#1a47c4] opacity-70 animate-ping' />
-        <span className='relative rounded-full bg-white p-2 shadow-lg'>
+        <span className='relative rounded-full bg-white shadow-lg'>
           <Image
             src='/ig-icon.png'
             alt='Instagram Icon'
             width={50}
             height={50}
-            className='rounded-full object-contain'
+            className='rounded-full object-contain size-full'
           />
         </span>
       </div>
