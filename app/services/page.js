31e38c320f67f36b63ad863a14f041ae846b3574
ebarronteodoro <ebarronteodoro@hotmail.com>
2025-04-16 15:32:59 -0500

    UPDATE: new design in services page

--- app/services/page.js
+++ app/services/page.js
@@ -4,7 +4,8 @@ import Link from 'next/link'
 export const metadata = {
   title: 'Servicios | Andrea Loarte Design Studio',
   description: 'Servicios de diseño gráfico y branding para tu marca.',
-  keywords: 'branding, diseño gráfico, servicios de diseño, identidad visual, marketing digital',
+  keywords:
+    'branding, diseño gráfico, servicios de diseño, identidad visual, marketing digital'
 }
 
 export default function ServicesPage () {
@@ -12,69 +13,89 @@ export default function ServicesPage () {
     {
       src: '/services/bg-1.png',
       title: 'Branding',
-      imageFront: ['/contacto/branding_request.png'],
-      description: 'Tu marca merece una identidad visual que impacte.',
+      serviceChats: [
+        '¡Por fin con mi negocio propio! Solo me falta el branding para despegar..'
+      ],
+      description: '“Tu marca merece una identidad visual que impacte.”',
       price: '$300',
-      color: '#b59894'
+      color: '#321d18',
+      bubbles: []
     },
     {
       src: '/services/bg-2.png',
       title: 'Rebranding',
-      imageFront: ['/contacto/rebranding_request.png'],
+      serviceChats: ['“Quiero mejorar la imagen de mi marca. ¡Ayudaa!”'],
       description: 'Transformemos tu historia para que impacte como nunca.',
       price: '$400',
-      color: '#3e241b'
+      color: '#321d18',
+      bubbles: []
     },
     {
       src: '/services/bg-3.png',
       title: 'Social Media',
-      imageFront: ['/contacto/socialmedia_request.png'],
+      serviceChats: [
+        '“Necesito que manejen el perfil de mi negocio y que mejore su imagen en redes sociales”'
+      ],
       description: 'Contenido visual alineado a tu marca.',
       price: '$300',
-      color: '#f2a5e6'
+      color: '#4d134c',
+      bubbles: ['Reels', 'Profile Feed']
     },
     {
       src: '/services/bg-4.png',
-      title: 'Web',
-      imageFront: [
-        '/contacto/web_request_2.png',
-        '/contacto/web_request_1.png'
+      title: 'Diseño Web',
+      serviceChats: [
+        '“Soy psicóloga y quiero un espacio donde mis clientes encuentren información sobre mis servicios”',
+        '“Tengo mi marca de ropa y me gustaría que mis clientes compren por la web”'
       ],
       description: 'Diseñamos landing y webs con venta en línea.',
       price: '$400',
-      color: '#768b88'
+      color: '#6b7f7e',
+      bubbles: ['Landing Page', 'Web + Shop']
     },
     {
       src: '/services/bg-5.png',
       title: 'Ilustración',
-      imageFront: ['/contacto/ilustracion_request.png'],
+      serviceChats: [
+        '“Estoy desarrollando un evento gastronómico y necesito una ilustración para hacer el merch del evento”'
+      ],
       description: 'Gráficos personalizados que cuentan historias.',
       price: '$100',
-      color: '#6c5c09'
+      color: '#7b8a73',
+      bubbles: ['Animación 2D', 'Digital', 'Impresión incluida']
     },
     {
       src: '/services/bg-6.png',
-      title: 'Consultoría Creativa',
-      imageFront: ['/contacto/consultoriacreativa_request.png'],
+      title: 'Fotografía',
+      serviceChats: [
+        '“Estoy por inaugurar mi cafetería pero necesito las fotos para mi contenido”'
+      ],
       description: 'Te guiamos para potenciar tu imagen y comunicación.',
-      price: '$30',
-      color: '#ae6a3f'
+      price: '$100',
+      color: '#877f30',
+      bubbles: ['Producto', 'Life Style']
     },
     {
       src: '/services/bg-7.png',
-      title: 'Fotografía',
-      imageFront: ['/contacto/fotografia_request.png'],
+      title: 'Packaging',
+      serviceChats: [
+        '“Soy #matchalover y quiero un empaque que refleje la esencia artesanal de mis postres con matcha”'
+      ],
       description: 'Imágenes que resaltan la esencia de tu marca.',
       price: '$100',
-      color: '#c0b233'
+      color: '#4d134c',
+      bubbles: []
     },
     {
       src: '/services/bg-8.png',
-      title: 'Packaging',
-      imageFront: ['/contacto/packaging_request.png'],
+      title: 'Consultoría Creativa',
+      serviceChats: [
+        '“Quiero que mi marca de velas se vea profesional y transmita mi esencia, pero no sé por dónde empezar..”'
+      ],
       description: 'Empaques que comunican valores y conectan con el cliente.',
-      price: '$100',
-      color: '#1c1647'
+      price: '$30',
+      color: '#a97448',
+      bubbles: []
     }
   ]
 
@@ -91,78 +112,74 @@ export default function ServicesPage () {
               className='relative w-full overflow-hidden h-full group'
             >
               <div className='relative h-full w-full transition-transform duration-500 md:group-hover:rotate-y-180'>
-                <div>
-                  <picture>
-                    <source
-                      media='(max-width: 768px)'
-                      srcSet={service.src.replace('.png', '-variant.png')}
-                    />
-                    <Image
-                      src={service.src}
-                      alt={service.title}
-                      width={500}
-                      height={500}
-                      className='object-cover'
-                    />
-                  </picture>
-                  <div className='absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center'>
-                    <div className='relative md:absolute md:inset-0 flex flex-col items-center justify-center gap-3 md:group-hover:opacity-0 transition-opacity duration-300 py-4'>
-                      <span
-                        style={{ color: service.color }}
-                        className='font-cardo text-4xl w-3/4 text-center leading-none mb-auto mt-6'
-                      >
-                        {service.title}
-                      </span>
-                      {service.title === 'Web' ? (
-                        <>
-                          <Image
-                            src={service.imageFront[0]}
-                            alt={service.title}
-                            width={500}
-                            height={500}
-                            className='w-[55%] mb-auto mr-auto ml-8'
-                          />
-                          <Image
-                            src={service.imageFront[1]}
-                            alt={service.title}
-                            width={500}
-                            height={500}
-                            className='hidden md:block w-[55%] -mt-5 mb-auto mr-8 ml-auto'
-                          />
-                        </>
-                      ) : service.title === 'Consultoría Creativa' ? (
-                        <Image
-                          src={service.imageFront[0]}
-                          alt={service.title}
-                          width={500}
-                          height={500}
-                          className='w-4/7 mb-auto'
-                        />
-                      ) : (
-                        <Image
-                          src={service.imageFront[0]}
-                          alt={service.title}
-                          width={500}
-                          height={500}
-                          className='w-5/7 mb-auto 2xl:w-7/10'
-                        />
-                      )}
-                    </div>
-
-                    <div
-                      className='relative md:absolute md:inset-0 flex flex-col gap-3 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300 md:rotate-y-180 font-cardo py-4 px-12'
-                      style={{ color: service.color }}
+                <picture>
+                  <source
+                    media='(max-width: 768px)'
+                    srcSet={service.src.replace('.png', '-variant.png')}
+                  />
+                  <Image
+                    src={service.src}
+                    alt={service.title}
+                    width={500}
+                    height={500}
+                    className='object-cover'
+                  />
+                </picture>
+                <div className='absolute top-0 left-0 w-full h-full flex flex-col md:justify-center'>
+                  <div className='relative md:absolute md:inset-0 flex flex-col items-center gap-3 md:group-hover:opacity-0 transition-opacity duration-300 py-0 [@media(min-width:90rem)]:py-1 px-5 [@media(min-width:90rem)]:px-6'>
+                    <span
+                      style={{
+                        color: service.color,
+                        borderColor: service.color
+                      }}
+                      className='font-cardo text-[31px] md:text-4xl w-full text-left leading-none border-b mt-6 pb-4 md:pb-2'
                     >
-                      <span className='font-cardo text-lg w-5/6 text-left text-balance leading-[1] md:mt-5 2xl:mt-0'>
-                        {service.description}
-                      </span>
-                      <span className='font-cardo flex mt-auto'>
-                        DESDE
-                        <strong className='text-[4.2rem] ml-auto mr-2'>
-                          {service.price}
-                        </strong>
-                      </span>
-                    </div>
+                      {service.title}
+                    </span>
+                    {service.serviceChats.length > 1 ? (
+                      <div className='flex gap-4 flex-col md:flex-row justify-between'>
+                        <p
+                          style={{ color: service.color }}
+                          className='font-cardo leading-[1.1] md:leading-[1.2] md:w-[45%] md:text-[15px] [@media(min-width:90rem)]:text-lg text-pretty md:text-balance'
+                        >
+                          {service.serviceChats[0]}
+                        </p>
+                        <p
+                          style={{ color: service.color }}
+                          className='font-cardo leading-[1.1] md:leading-[1.2] w-[70%] ml-auto md:ml-0 md:w-[45%] [@media(min-width:90rem)]:text-lg text-pretty md:text-balance pb-6 md:pb-0'
+                        >
+                          {service.serviceChats[1]}
+                        </p>
+                      </div>
+                    ) : (
+                      service.serviceChats.map((chat, chatIndex) => (
+                        <p
+                          key={chatIndex}
+                          style={{ color: service.color }}
+                          className={`font-cardo leading-[1.1] md:leading-[1.2] text-[22px] w-[90%] md:w-full mx-auto md:mx-0 md:px-0 pb-6 md:pb-0 [@media(min-width:90rem)]:text-2xl md:text-pretty ${service.bubbles.length < 1 && '[@media(min-width:90rem)]:pt-3 [@media(min-width:90rem)]:text-[26px]'}`}
+                        >
+                          {chat}
+                        </p>
+                      ))
+                    )}
+                  </div>
+
+                  <div
+                    className='relative md:absolute md:inset-0 flex flex-col gap-3 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300 md:rotate-y-180 font-cardo py-0 [@media(min-width:90rem)]:py-1 px-5 [@media(min-width:90rem)]:px-6 h-full md:h-auto'
+                    style={{ color: service.color }}
+                  >
+                    <span
+                      className='font-cardo text-xl w-full pt-7 md:pt-0 px-3 md:px-0 md:w-5/6 text-left text-balance leading-[1] md:mt-6 [@media(min-width:90rem)]:mt-10 border-t md:border-t-0'
+                      style={{ borderColor: service.color }}
+                    >
+                      {service.description}
+                    </span>
+                    <span className='font-cardo flex text-sm px-3 md:px-0 pt-6 [@media(min-width:90rem)]:text-base mt-auto mb-7 md:mb-6'>
+                      DESDE
+                      <strong className='text-6xl px-5 border rounded-4xl ml-auto mr-2 font-normal'>
+                        {service.price}
+                      </strong>
+                    </span>
                   </div>
                 </div>
               </div>
