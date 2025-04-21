9dcd1fef964a4c884185ffc07aa01de288c78e89
ebarronteodoro <ebarronteodoro@hotmail.com>
2025-04-21 14:03:20 -0500

    UPDATE: Barra en project page

--- app/gallery/[slug]/page.js
+++ app/gallery/[slug]/page.js
@@ -75,24 +75,23 @@ export default async function GalleryPage ({ params }) {
       {project.firstImage && (
         <section className='container mx-auto px-4 mt-16'>
           <div className='max-h-[847.91px] overflow-hidden relative flex items-center justify-center'>
-            {['.mp4', '.webm', '.mov'].some(ext => project.firstImage.toLowerCase().endsWith(ext)) ? (
+            {['.mp4', '.webm', '.mov'].some(ext =>
+              project.firstImage.toLowerCase().endsWith(ext)
+            ) ? (
               <video
                 autoPlay
                 loop
                 controls
                 playsInline
-                preload="auto"
+                preload='auto'
                 className='w-full h-full object-cover relative animate-fade-up animate-delay-200'
                 style={{ maxHeight: '847.91px' }}
               >
-                <source 
-                  src={project.firstImage} 
+                <source
+                  src={project.firstImage}
                   type={`video/${project.firstImage.split('.').pop()}`}
                 />
-                <source 
-                  src={project.firstImage} 
-                  type="video/mp4"
-                />
+                <source src={project.firstImage} type='video/mp4' />
                 Tu navegador no soporta el elemento de video.
               </video>
             ) : (
@@ -130,15 +129,33 @@ export default async function GalleryPage ({ params }) {
 
       {Componente && <Componente />}
 
+      <section class='wrapper py-8 mt-8'>
+        <div class='animate-fade-up animate-delay-300'>
+          <div class='md:flex md:items-center max-w-[1440px] md:mx-auto md:justify-center'>
+            <h3 class='font-cardo text-white text-center text-2xl px-8 md:text-5xl'>
+              Quiero Empezar A Construir Mi Obra
+            </h3>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              class='mx-auto text-center block w-fit bg-white text-[16px] mt-4 px-4 py-2 shadow-[0px_14px_36px_rgba(0,0,0,0.5)] text-[#292605] md:m-0 rounded-[10px] hover:bg-[#1a47c4] transition all hover:text-white'
+              href='https://docs.google.com/forms/d/e/1FAIpQLScJ5p8uYDnV24f9Ivg-PVMUNG70N1XbUvGZCRw0lXMoATVxuA/viewform?usp=sharing'
+            >
+              Agenda Aquí
+            </a>
+          </div>
+        </div>
+      </section>
+
       {nextProject && (
-        <section className='container mx-auto px-4 mt-16 animate-fade-up border-t border-gray-200 pt-4 max-h-[100dvh] overflow-hidden group'>
+        <section className='container mx-auto px-4 animate-fade-up border-t border-gray-200 pt-4 max-h-[100dvh] overflow-hidden group'>
           <Link href={`/gallery/${nextProject.slug}`}>
             <h2 className='font-medium mb-8 text-lg text-[#1a47c4]'>
               Siguiente Proyecto
             </h2>
             <div className='flex flex-col md:flex-row md:justify-between relative cursor-pointer'>
               <div className='w-full'>
-                <h1 className='text-4xl md:text-[55px] font-medium -mt-12 md:mt-0 mb-8 md:mb-0'>
+                <h1 className='text-4xl md:text-[55px] font-medium mt-0 mb-4 md:mb-0'>
                   {nextProject.name}
                 </h1>
                 <span className='md:text-lg 2xl:text-2xl flex md:w-80 text-balance text-gray-400'>
@@ -153,25 +170,24 @@ export default async function GalleryPage ({ params }) {
             </div>
             {nextProject.firstImage && (
               <div className='max-h-[847.91px] overflow-hidden relative flex items-center justify-center mt-16'>
-                {['.mp4', '.webm', '.mov'].some(ext => nextProject.firstImage.toLowerCase().endsWith(ext)) ? (
+                {['.mp4', '.webm', '.mov'].some(ext =>
+                  nextProject.firstImage.toLowerCase().endsWith(ext)
+                ) ? (
                   <video
                     autoPlay
                     loop
                     controls
                     playsInline
                     muted
-                    preload="auto"
+                    preload='auto'
                     className='w-full h-full object-cover relative animate-fade-up animate-delay-200 group-hover:scale-105 transition-transform duration-300'
                     style={{ maxHeight: '847.91px' }}
                   >
-                    <source 
-                      src={nextProject.firstImage} 
+                    <source
+                      src={nextProject.firstImage}
                       type={`video/${nextProject.firstImage.split('.').pop()}`}
                     />
-                    <source 
-                      src={nextProject.firstImage} 
-                      type="video/mp4"
-                    />
+                    <source src={nextProject.firstImage} type='video/mp4' />
                     Tu navegador no soporta el elemento de video.
                   </video>
                 ) : (
