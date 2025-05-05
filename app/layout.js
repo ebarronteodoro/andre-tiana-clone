8369d7ea5ff0dc27368b34daf647a2c0e8ea0a69
ebarronteodoro <ebarronteodoro@hotmail.com>
2025-05-05 10:00:46 -0500

    UPDATE: Add favicon metadata for improved branding and user experience

--- app/layout.js
+++ app/layout.js
@@ -145,7 +145,6 @@ const timesNewRoman = localFont({
   display: 'swap'
 })
 
-// Agregamos Cardo de forma similar
 const cardo = localFont({
   src: [
     {
@@ -168,6 +167,12 @@ const cardo = localFont({
   display: 'swap'
 })
 
+export const metadata = {
+  icons: {
+    icon: '/favicon-192x192.ico'
+  }
+}
+
 export default function RootLayout ({ children }) {
   return (
     <html
@@ -182,9 +187,6 @@ export default function RootLayout ({ children }) {
         cardo.variable
       }
     >
-      <head>
-        <link rel='icon' href='/favicon/favicon.ico' sizes='any' />
-      </head>
       <body className='!relative'>
         <Header />
         {children}
